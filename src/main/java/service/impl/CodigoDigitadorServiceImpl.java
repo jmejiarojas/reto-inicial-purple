94cff1a30e01dca84a9250605773b1653ddf82fb
Julio Mejia <julioedgarmejiarojas338@gmail.com>
2022-01-10 14:23:05 -0500

    fix: multiplicar

--- src/main/java/service/impl/CodigoDigitadorServiceImpl.java
+++ src/main/java/service/impl/CodigoDigitadorServiceImpl.java
@@ -4,13 +4,21 @@ import dto.api.DniRequest;
 import service.CodigoDigitadorService;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class CodigoDigitadorServiceImpl implements CodigoDigitadorService {
 
     @Override
     public String generateCodigo(DniRequest request) {
 
-        Arrays.stream(request.getNumero().split(""))
+        List<Integer> multiplos = Arrays.asList(3, 2, 7, 6, 5, 4, 3, 2);
+        Integer suma = Arrays.stream(request.getNumero().split(""))
+                .flatMap(s -> multiplos.stream().map(
+                        integer -> Integer.parseInt(s) * integer
+                )).reduce(0, Integer::sum);
+        int residuo = suma % 11;
         return null;
     }
 }
